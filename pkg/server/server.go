72e80eb535ed0e35de2c31eff5644f5b62a51134
林　遼太朗 <hys.rtr.forit@gmail.com>
2021-04-09 01:27:11 +0900

    hello worldを追加

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -7,6 +7,7 @@ import (
 	"InfecShotAPI/pkg/server/handler"
 	"InfecShotAPI/pkg/server/model"
 	"InfecShotAPI/pkg/server/service"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -33,6 +34,10 @@ func Serve(addr string) {
 	http.HandleFunc("/user/get", accessMiddleware.Access(get(authMiddleware.Authenticate(userHandler.HandleUserGet))))
 	http.HandleFunc("/game/finish", accessMiddleware.Access(post(authMiddleware.Authenticate(gameHandler.HandleGameFinish))))
 	http.HandleFunc("/ranking/list", accessMiddleware.Access(get(rankingHandler.HandleRankingList)))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
 
 	/* ===== サーバの起動 ===== */
 	log.Println("Server running...")
